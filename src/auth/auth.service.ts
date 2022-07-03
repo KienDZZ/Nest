e8608b95c9b87d83ed577d1562b9e373d1bc1f9e
kien.letrung <kienle26074@gmail.com>
2022-07-04 00:12:33 +0700

    4/7

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import { PrismaService } from './../prisma/prisma.service';
+import { Injectable } from "@nestjs/common";
+import { AuthDto } from './dto';
+import * as argon from 'argon2';
+
+
+@Injectable()
+export class AuthService{
+   constructor(private prisma: PrismaService) {
+       
+   }
+
+   async signup(dto: AuthDto){
+    const hash = await argon.hash(dto.password);
+
+    const user = await this.prisma.user.create({
+      data:{
+        email: dto.email,
+        password: hash,
+      }, // this is the end of the day 4/7/2022
+    });
+    return user;
+}
+   
+    signin(){
+      return {message: 'i am signin'}
+    }
+
+    
+}
